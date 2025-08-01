56b8d0362d01cda36be51e1771a870ce4c0e6c32
il-way <ilway@naver.com>
2025-08-02 00:37:11 +0900

    fix: application > CloudOperation 사용토록 수정
      - application > MetricField에 CloudOperation 사용하도록 수정
      - application > CloudConditionPredicate에 CloudOperation 사용하도록 수정

--- application/src/main/java/model/weather/CloudConditionPredicate.java
+++ application/src/main/java/model/weather/CloudConditionPredicate.java
@@ -1,6 +1,7 @@
 package model.weather;
 
 import lombok.RequiredArgsConstructor;
+import service.CloudOperation;
 import vo.weather.CloudGroup;
 import vo.weather.type.CloudCoverage;
 import vo.weather.type.CloudType;
@@ -12,11 +13,11 @@ import java.util.function.BiPredicate;
 public enum CloudConditionPredicate {
 
   HAS_CLOUDTYPE((cg, target) -> {
-    return cg.containsCloudType(cast(target, CloudType.class));
+    return CloudOperation.containsCloudType(cg, cast(target, CloudType.class));
   }),
 
   HAS_COVERAGE((cg, target) -> {
-    return cg.containsCloudCoverage(cast(target, CloudCoverage.class));
+    return CloudOperation.containsCloudCoverage(cg, cast(target, CloudCoverage.class));
   });
 
   private final BiPredicate<CloudGroup, WeatherDescription> tester;
