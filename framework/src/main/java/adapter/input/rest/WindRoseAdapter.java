d41b8e75e1ca1e2f6eae38b045757831d44366d4
il-way <ilway@naver.com>
2025-08-18 20:26:43 +0900

    feat: framework > WindRoseAdapter 추가

---
+++ framework/src/main/java/adapter/input/rest/WindRoseAdapter.java
@@ -0,0 +1,43 @@
+package adapter.input.rest;
+
+import dto.RetrievalPeriod;
+import dto.windrose.DirectionBin;
+import dto.windrose.SpeedBin;
+import dto.windrose.WindRose;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import usecase.WindRoseUseCase;
+
+import java.time.Month;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/windrose")
+@RestController
+@RequiredArgsConstructor
+public class WindRoseAdapter {
+
+	private final WindRoseUseCase windRoseUseCase;
+
+	@GetMapping("/{icao}")
+	public ResponseEntity<Map<Month, WindRose>> getWindRose(
+		@PathVariable("icao") String icao,
+		@RequestParam("startDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime st,
+		@RequestParam("endDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime ed
+	) {
+		List<SpeedBin> speedBins = SpeedBin.of5KtSpeedBins();
+		List<DirectionBin> directionBins = DirectionBin.of16DirectionBins();
+		Map<Month, WindRose> windRose = windRoseUseCase.generateMonthlyWindRose(
+			icao,
+			new RetrievalPeriod(st, ed),
+			speedBins,
+			directionBins
+		);
+
+		return ResponseEntity.ok()
+			       .body(windRose);
+	}
+}
