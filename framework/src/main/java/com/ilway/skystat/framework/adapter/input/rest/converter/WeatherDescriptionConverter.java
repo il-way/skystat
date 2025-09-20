c7062756205805ed7f72466d296d5f3c64917710
il-way <ilway@naver.com>
2025-09-20 20:57:03 +0900

    feat: framework > MetarStatisticAdapterTest 추가

---
+++ framework/src/main/java/com/ilway/skystat/framework/adapter/input/rest/converter/WeatherDescriptionConverter.java
@@ -0,0 +1,33 @@
+package com.ilway.skystat.framework.adapter.input.rest.converter;
+
+import com.ilway.skystat.domain.vo.weather.type.*;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WeatherDescriptionConverter implements Converter<String, WeatherDescription> {
+
+	@Override
+	public WeatherDescription convert(String source) {
+		if (source == null) return null;
+		String s = source.trim().toUpperCase();
+
+		for (WeatherDescriptor wd : WeatherDescriptor.values()) {
+			if (wd.name().equals(s)) return wd;
+		}
+
+		for (WeatherPhenomenon wd : WeatherPhenomenon.values()) {
+			if (wd.name().equals(s)) return wd;
+		}
+
+		for (CloudType wd : CloudType.values()) {
+			if (wd.name().equals(s)) return wd;
+		}
+
+		for (CloudCoverage wd : CloudCoverage.values()) {
+			if (wd.name().equals(s)) return wd;
+		}
+
+		throw new IllegalArgumentException("Unsupported unit: " + source);
+	}
+}
--- framework/src/test/java/com/ilway/skystat/framework/adapter/input/MetarStatisticAdapterTest.java
+++ framework/src/test/java/com/ilway/skystat/framework/adapter/input/MetarStatisticAdapterTest.java
@@ -1,28 +1,22 @@
 package com.ilway.skystat.framework.adapter.input;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ilway.skystat.application.port.input.metar.MetarManagementInputPort;
-import com.ilway.skystat.application.usecase.MetarManagementUseCase;
-import com.ilway.skystat.framework.adapter.input.rest.converter.ComparisonConverter;
-import com.ilway.skystat.framework.adapter.input.rest.converter.UnitConverter;
+import com.ilway.skystat.application.dto.statistic.ObservationStatisticResponse;
 import com.ilway.skystat.framework.adapter.output.file.ResourceFileConfig;
-import com.ilway.skystat.framework.adapter.output.resource.MetarManagementResourceFileAdapter;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.transaction.annotation.Transactional;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
 @SpringBootTest(classes = {ResourceFileConfig.class})
@@ -30,24 +24,209 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Transactional
 public class MetarStatisticAdapterTest {
 
-	@Autowired MockMvc mockMvc;
-	@Autowired ObjectMapper objectMapper;
+	@Autowired
+	MockMvc mockMvc;
+	@Autowired
+	ObjectMapper objectMapper;
 
 	@Test
-	void getThresholdStatisticTestSuccess() throws Exception {
+	@DisplayName("peakwind 조건 조회에 성공해야 한다.")
+	void getPeakWindThresholdStatisticTestSuccess() throws Exception {
 		String icao = "RKSI";
-		mockMvc.perform(get("/metar/statistic/threshold/{icao}", icao)
-			                .param("field", "peakwind")
-			                .param("comparison", "GTE")
-			                .param("threshold", "25")
-			                .param("unit", "KT")
-			                .param("startDateTime", "2019-01-01T00:00:00Z")
-			                .param("endDateTime", "2025-01-01T00:00:00Z")
-			                .accept(MediaType.APPLICATION_JSON)
-			)
-			.andExpect(status().isOk());
+		MvcResult mvcResult = mockMvc.perform(get("/metar/statistic/threshold/{icao}", icao)
+			                                      .param("field", "peakwind")
+			                                      .param("comparison", "GTE")
+			                                      .param("threshold", "25")
+			                                      .param("unit", "KT")
+			                                      .param("startDateTime", "2019-01-01T00:00:00Z")
+			                                      .param("endDateTime", "2025-01-01T00:00:00Z")
+			                                      .accept(MediaType.APPLICATION_JSON))
+			                      .andExpect(status().isOk())
+			                      .andReturn();
 
+		ObservationStatisticResponse response = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(UTF_8),
+			ObservationStatisticResponse.class
+		);
+	}
 
+	@Test
+	@DisplayName("lowest ceiling 조건 조회에 성공해야 한다.")
+	void getCeilingThresholdStatisticTestSuccess() throws Exception {
+		String icao = "RKSI";
+		MvcResult mvcResult = mockMvc.perform(get("/metar/statistic/threshold/{icao}", icao)
+			                                      .param("field", "ceiling")
+			                                      .param("comparison", "lte")
+			                                      .param("threshold", "500")
+			                                      .param("unit", "feet")
+			                                      .param("startDateTime", "2019-01-01T00:00:00Z")
+			                                      .param("endDateTime", "2024-01-01T00:00:00Z")
+			                                      .accept(MediaType.APPLICATION_JSON))
+			                      .andExpect(status().isOk())
+			                      .andReturn();
+
+		ObservationStatisticResponse response = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(UTF_8),
+			ObservationStatisticResponse.class
+		);
+	}
+
+	@Test
+	@DisplayName("wind speed 조건 조회에 성공해야 한다.")
+	void getWindSpeedThresholdStatisticTestSuccess() throws Exception {
+		String icao = "RKSI";
+		MvcResult mvcResult = mockMvc.perform(get("/metar/statistic/threshold/{icao}", icao)
+			                                      .param("field", "windspeed")
+			                                      .param("comparison", "gte")
+			                                      .param("threshold", "15")
+			                                      .param("unit", "kt")
+			                                      .param("startDateTime", "2019-01-01T00:00:00Z")
+			                                      .param("endDateTime", "2024-01-01T00:00:00Z")
+			                                      .accept(MediaType.APPLICATION_JSON))
+			                      .andExpect(status().isOk())
+			                      .andReturn();
+
+		ObservationStatisticResponse response = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(UTF_8),
+			ObservationStatisticResponse.class
+		);
+	}
+
+	@Test
+	@DisplayName("visibility 조건 조회에 성공해야 한다.")
+	void getVisisbilityThresholdStatisticTestSuccess() throws Exception {
+		String icao = "RKSI";
+		MvcResult mvcResult = mockMvc.perform(get("/metar/statistic/threshold/{icao}", icao)
+			                                      .param("field", "visibility")
+			                                      .param("comparison", "lte")
+			                                      .param("threshold", "800")
+			                                      .param("unit", "meters")
+			                                      .param("startDateTime", "2019-01-01T00:00:00Z")
+			                                      .param("endDateTime", "2024-01-01T00:00:00Z")
+			                                      .accept(MediaType.APPLICATION_JSON))
+			                      .andExpect(status().isOk())
+			                      .andReturn();
+
+		ObservationStatisticResponse response = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(UTF_8),
+			ObservationStatisticResponse.class
+		);
+	}
+
+	@Test
+	@DisplayName("altimeter 조건 조회에 성공해야 한다.")
+	void getAltimeterThresholdStatisticTestSuccess() throws Exception {
+		String icao = "RKSI";
+		MvcResult mvcResult = mockMvc.perform(get("/metar/statistic/threshold/{icao}", icao)
+			                                      .param("field", "altimeter")
+			                                      .param("comparison", "lte")
+			                                      .param("threshold", "30")
+			                                      .param("unit", "inhg")
+			                                      .param("startDateTime", "2019-01-01T00:00:00Z")
+			                                      .param("endDateTime", "2024-01-01T00:00:00Z")
+			                                      .accept(MediaType.APPLICATION_JSON))
+			                      .andExpect(status().isOk())
+			                      .andReturn();
+
+		ObservationStatisticResponse response = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(UTF_8),
+			ObservationStatisticResponse.class
+		);
+	}
+
+	@Test
+	@DisplayName("cloud type 조회에 성공해야 한다.")
+	void getCloudTypeStatisticTestSuccess() throws Exception {
+		String icao = "RKSI";
+		MvcResult mvcResult = mockMvc.perform(get("/metar/statistic/cloud/{icao}", icao)
+			                                      .param("condition", "type")
+			                                      .param("target", "CB")
+			                                      .param("startDateTime", "2019-01-01T00:00:00Z")
+			                                      .param("endDateTime", "2024-01-01T00:00:00Z")
+			                                      .accept(MediaType.APPLICATION_JSON))
+			                      .andExpect(status().isOk())
+			                      .andReturn();
+
+		ObservationStatisticResponse response = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(UTF_8),
+			ObservationStatisticResponse.class
+		);
+	}
+
+	@Test
+	@DisplayName("cloud coverage 조회에 성공해야 한다.")
+	void getCloudCoverageStatisticTestSuccess() throws Exception {
+		String icao = "RKSI";
+		MvcResult mvcResult = mockMvc.perform(get("/metar/statistic/cloud/{icao}", icao)
+			                                      .param("condition", "coverage")
+			                                      .param("target", "bkn")
+			                                      .param("startDateTime", "2019-01-01T00:00:00Z")
+			                                      .param("endDateTime", "2024-01-01T00:00:00Z")
+			                                      .accept(MediaType.APPLICATION_JSON))
+			                      .andExpect(status().isOk())
+			                      .andReturn();
+
+		ObservationStatisticResponse response = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(UTF_8),
+			ObservationStatisticResponse.class
+		);
+	}
+
+	@Test
+	@DisplayName("weather 조회에 성공해야 한다.")
+	void getWeatherBothCoverageStatisticTestSuccess() throws Exception {
+		String icao = "RKSI";
+		MvcResult mvcResult = mockMvc.perform(get("/metar/statistic/weather/{icao}", icao)
+			                                      .param("condition", "both")
+			                                      .param("list", "TS,RA")
+			                                      .param("startDateTime", "2019-01-01T00:00:00Z")
+			                                      .param("endDateTime", "2024-01-01T00:00:00Z")
+			                                      .accept(MediaType.APPLICATION_JSON))
+			                      .andExpect(status().isOk())
+			                      .andReturn();
+
+		ObservationStatisticResponse response = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(UTF_8),
+			ObservationStatisticResponse.class
+		);
+	}
+
+	@Test
+	@DisplayName("weather descriptor 조회에 성공해야 한다.")
+	void getWeatherDescriptorCoverageStatisticTestSuccess() throws Exception {
+		String icao = "RKSI";
+		MvcResult mvcResult = mockMvc.perform(get("/metar/statistic/weather/{icao}", icao)
+			                                      .param("condition", "descriptor")
+			                                      .param("list", "TS")
+			                                      .param("startDateTime", "2019-01-01T00:00:00Z")
+			                                      .param("endDateTime", "2024-01-01T00:00:00Z")
+			                                      .accept(MediaType.APPLICATION_JSON))
+			                      .andExpect(status().isOk())
+			                      .andReturn();
+
+		ObservationStatisticResponse response = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(UTF_8),
+			ObservationStatisticResponse.class
+		);
+	}
+
+	@Test
+	@DisplayName("weather phenomena 조회에 성공해야 한다.")
+	void getWeatherPhenomenaCoverageStatisticTestSuccess() throws Exception {
+		String icao = "RKSI";
+		MvcResult mvcResult = mockMvc.perform(get("/metar/statistic/weather/{icao}", icao)
+			                                      .param("condition", "phenomena")
+			                                      .param("list", "SN,RA")
+			                                      .param("startDateTime", "2019-01-01T00:00:00Z")
+			                                      .param("endDateTime", "2024-01-01T00:00:00Z")
+			                                      .accept(MediaType.APPLICATION_JSON))
+			                      .andExpect(status().isOk())
+			                      .andReturn();
+
+		ObservationStatisticResponse response = objectMapper.readValue(
+			mvcResult.getResponse().getContentAsString(UTF_8),
+			ObservationStatisticResponse.class
+		);
 	}
 
 }
