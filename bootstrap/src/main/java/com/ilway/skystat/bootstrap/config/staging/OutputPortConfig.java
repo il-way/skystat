6d36ce0a87fe0192a4340d23425324540be115da
il-way <ilway@naver.com>
2025-10-04 10:34:51 +0900

    fix: bootstrap > Config 수정  - UseCaseConfig: FallbackInputPort로 변경  - OutputPortConfig: StatisticQuery용 outputPort 추가

--- bootstrap/src/main/java/com/ilway/skystat/bootstrap/config/staging/OutputPortConfig.java
+++ bootstrap/src/main/java/com/ilway/skystat/bootstrap/config/staging/OutputPortConfig.java
@@ -1,10 +1,19 @@
 package com.ilway.skystat.bootstrap.config.staging;
 
+import com.ilway.skystat.application.port.output.CloudStatisticQueryOutputPort;
 import com.ilway.skystat.application.port.output.MetarManagementOutputPort;
+import com.ilway.skystat.application.port.output.ThresholdStatisticQueryOutputPort;
+import com.ilway.skystat.application.port.output.WeatherStatisticQueryOutputPort;
 import com.ilway.skystat.bootstrap.profile.Production;
 import com.ilway.skystat.bootstrap.profile.Staging;
 import com.ilway.skystat.framework.adapter.output.mysql.MetarManagementMySQLAdapter;
+import com.ilway.skystat.framework.adapter.output.mysql.repository.MetarCloudQueryRepository;
 import com.ilway.skystat.framework.adapter.output.mysql.repository.MetarManagementRepository;
+import com.ilway.skystat.framework.adapter.output.mysql.repository.MetarMetricQueryRepository;
+import com.ilway.skystat.framework.adapter.output.mysql.repository.MetarWeatherQueryRepository;
+import com.ilway.skystat.framework.adapter.output.mysql.statistic.CloudStatisticQueryMySqlAdapter;
+import com.ilway.skystat.framework.adapter.output.mysql.statistic.ThresholdStatisticQueryMySqlAdapter;
+import com.ilway.skystat.framework.adapter.output.mysql.statistic.WeatherStatisticQueryMySqlAdapter;
 import jakarta.persistence.EntityManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +27,21 @@ public class OutputPortConfig {
 		MetarManagementRepository repo,
 		EntityManager em) {
 		return new MetarManagementMySQLAdapter(repo, em);
+	}
+
+	@Bean
+	public CloudStatisticQueryOutputPort cloudStatisticQueryOutputPort(MetarCloudQueryRepository cloudQueryRepository) {
+		return new CloudStatisticQueryMySqlAdapter(cloudQueryRepository);
+	}
+
+	@Bean
+	public WeatherStatisticQueryOutputPort weatherStatisticQueryOutputPort(MetarWeatherQueryRepository weatherQueryRepository) {
+		return new WeatherStatisticQueryMySqlAdapter(weatherQueryRepository);
+	}
+
+	@Bean
+	public ThresholdStatisticQueryOutputPort thresholdStatisticQueryOutputPort(MetarMetricQueryRepository metricQueryRepository) {
+		return new ThresholdStatisticQueryMySqlAdapter(metricQueryRepository);
 	}
 
 
