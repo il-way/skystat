117a5e6e4d350f5016375d618b380ba8fc2b7e06
il-way <ilway@naver.com>
2025-10-07 15:29:39 +0900

    feat: bootstrap > config 수정  - OutputPortConfig, UseCaseConfig에 Temperature관련 빈 추가

--- bootstrap/src/main/java/com/ilway/skystat/bootstrap/config/staging/UseCaseConfig.java
+++ bootstrap/src/main/java/com/ilway/skystat/bootstrap/config/staging/UseCaseConfig.java
@@ -5,21 +5,18 @@ import com.ilway.skystat.application.dto.statistic.ThresholdStatisticQuery;
 import com.ilway.skystat.application.dto.statistic.WeatherStatisticQuery;
 import com.ilway.skystat.application.port.input.metar.*;
 import com.ilway.skystat.application.port.input.metar.fallback.CloudStatisticFallbackInputPort;
+import com.ilway.skystat.application.port.input.metar.fallback.TemperatureStatisticFallbackInputPort;
 import com.ilway.skystat.application.port.input.metar.fallback.ThresholdStatisticFallbackInputPort;
 import com.ilway.skystat.application.port.input.metar.fallback.WeatherStatisticFallbackInputPort;
 import com.ilway.skystat.application.port.input.metar.query.CloudStatisticQueryInputPort;
+import com.ilway.skystat.application.port.input.metar.query.TemperatureStatisticQueryInputPort;
 import com.ilway.skystat.application.port.input.metar.query.ThresholdStatisticQueryInputPort;
 import com.ilway.skystat.application.port.input.metar.query.WeatherStatisticQueryInputPort;
-import com.ilway.skystat.application.port.input.metar.scan.CloudStatisticInputPort;
-import com.ilway.skystat.application.port.input.metar.scan.ThresholdStatisticInputPort;
-import com.ilway.skystat.application.port.input.metar.scan.WeatherStatisticInputPort;
-import com.ilway.skystat.application.port.input.metar.scan.WindRoseInputPort;
-import com.ilway.skystat.application.port.output.CloudStatisticQueryOutputPort;
-import com.ilway.skystat.application.port.output.MetarManagementOutputPort;
-import com.ilway.skystat.application.port.output.ThresholdStatisticQueryOutputPort;
-import com.ilway.skystat.application.port.output.WeatherStatisticQueryOutputPort;
+import com.ilway.skystat.application.port.input.metar.scan.*;
+import com.ilway.skystat.application.port.output.*;
 import com.ilway.skystat.application.usecase.MetarManagementUseCase;
 import com.ilway.skystat.application.usecase.StatisticUseCase;
+import com.ilway.skystat.application.usecase.TemperatureStatisticUseCase;
 import com.ilway.skystat.application.usecase.WindRoseUseCase;
 import com.ilway.skystat.bootstrap.profile.Staging;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +25,15 @@ import org.springframework.context.annotation.Configuration;
 @Staging
 @Configuration
 public class UseCaseConfig {
+
+	@Bean
+	public TemperatureStatisticUseCase temperatureStatisticUseCase(MetarManagementOutputPort metarManagementOutputPort,
+	                                                               TemperatureStatisticQueryOutputPort temperatureStatisticQueryOutputPort) {
+		return new TemperatureStatisticFallbackInputPort(
+			new TemperatureStatisticInputPort(metarManagementOutputPort),
+			new TemperatureStatisticQueryInputPort(temperatureStatisticQueryOutputPort)
+		);
+	}
 
 	@Bean
 	public StatisticUseCase<ThresholdStatisticQuery> thresholdStatisticUseCase(MetarManagementOutputPort metarManagementOutputPort,
